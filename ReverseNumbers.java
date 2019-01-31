e685dde6e528fbf1d918bc3c4655ac783de54b21
Aare Palm <aarepalm@gmail.com>
2019-01-31 10:49:47 +0200

    Update ReverseNumbers.java

--- ReverseNumbers.java
+++ ReverseNumbers.java
@@ -7,7 +7,7 @@ class ReverseNumbers {
 		do {
 			// Print out the remainder, for example  7532%10 == 2
 			System.out.print(numberToReverse%10);
-			// "Shift" the number to right (or chop off the rightmost digit), for example, 7532/10 == 753
+			// "Shift" the number to right, for example, 7532/10 == 753
 			numberToReverse = numberToReverse/10;
 
 			// NB! Repeat the statements above until the number is all printed reversed
@@ -22,13 +22,8 @@ class ReverseNumbers {
 
         ///////////////////////
         //////////// Solution 2
-       private void reverseNumberRecursive(int numberToReverse) {
-	       	// Print out the single, for example the 2 from 7532
+        private void reverseNumberRecursive(int numberToReverse) {
 		System.out.print(numberToReverse%10);
-	       	// If the numberToReverse has more than one digit then chop off the single (and making the
-	       	// the number one digit shorter) and call the same function again.
-	       	// Note that when we are calling the function with last digit, the 7 in our example, it is printed
-	       	// and the recursion stops.
 		if (numberToReverse > 9)
 	        	reverseNumberRecursive(numberToReverse/10);
         }
@@ -36,22 +31,30 @@ class ReverseNumbers {
         ///////////////////////
         //////////// Solution 3
 	private void reverseNumberWithStringConversion(int numberToReverse) {
-		// Convert the numberToReverse to String so we could index all characters individually
 		String intAsString = Integer.toString(numberToReverse);
 		int stringLength = intAsString.length();
 		while (stringLength != 0) {
-			// Note that the stringLength is first decremented and the then new value is used in the expression
 			System.out.print(intAsString.charAt(--stringLength));
 		}
 	}
 
+        ///////////////////////
+        //////////// Solution 4
+        public static void reverseNumberRecursiveStatic(int numberToReverse) {
+                System.out.print(numberToReverse%10);
+                if (numberToReverse > 9)
+                        reverseNumberRecursiveStatic(numberToReverse/10);
+        }
+
+
 	public static void main(String[] args) {
-		ReverseNumbers reverser = new ReverseNumbers();
 		int numberFromUserInput = 13579; // This could be read from user input
 
-		/////////////////////////////////////////////////////////////////////////////////
-		//////// This section is just for testing that all works with multi-digit numbers
-		System.out.println(" ---------- Reversing a number that is at least 2 digits ----------------");
+	  	System.out.println("Reversing " + numberFromUserInput + " with static method");
+		ReverseNumbers.reverseNumberRecursiveStatic(numberFromUserInput);
+
+		ReverseNumbers reverser = new ReverseNumbers();
+		System.out.println("\n ---------- Reversing a number that is at least 2 digits ----------------");
 		System.out.println("Reversing " + numberFromUserInput);
 		// Notice the \n, this makes a line change in output
 		System.out.println("\nNon-recursive");
@@ -61,8 +64,7 @@ class ReverseNumbers {
                 System.out.println("\nConverting to string");
                 reverser.reverseNumberWithStringConversion(numberFromUserInput);
 
-		/////////////////////////////////////////////////////////////////////////////////
-		//////// This section is just for testing that all works with number 0
+
 		numberFromUserInput = 0;
                 System.out.println("\n\n ---------- Reversing a 0 (must not crash :-) ---------------------------");
                 System.out.println("Reversing " + numberFromUserInput);
@@ -74,8 +76,6 @@ class ReverseNumbers {
                 System.out.println("\nConverting to string");
                 reverser.reverseNumberWithStringConversion(numberFromUserInput);
 
-		/////////////////////////////////////////////////////////////////////////////////
-		//////// This section is just for testing that all works with single-digit numbers
 		numberFromUserInput = 4;
                 System.out.println("\n\n ---------- Reversing a single digit number (must not crash :-) --------");
                 System.out.println("Reversing " + numberFromUserInput);
